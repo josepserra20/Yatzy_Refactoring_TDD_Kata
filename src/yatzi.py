29dcdefadc6b643d4baf2ce9c0432779850dcbea
josepserra20 <jserravives@cifpfbmoll.eu>
2021-12-28 17:17:42 +0100

    refactor: :recycle: refacttorizar
    refactorizar completado

--- src/yatzi.py
+++ src/yatzi.py
@@ -43,166 +43,72 @@ class Yatzy:
     def sixes(self):
         SIX = 6
         return self.dice.count(SIX) * SIX
-    
-
-    def __init__(self, d1, d2, d3, d4, _5):
-        self.dice = [0]*5
-        self.dice[0] = d1
-        self.dice[1] = d2
-        self.dice[2] = d3
-        self.dice[3] = d4
-        self.dice[4] = _5
-    
-    def fours(self):
-        sum = 0
-        for at in range(5):
-            if (self.dice[at] == 4): 
-                sum += 4
-        return sum
-    
-
-    def fives(self):
-        s = 0
-        i = 0
-        for i in range(len(self.dice)): 
-            if (self.dice[i] == 5):
-                s = s + 5
-        return s
-    
-
-    def sixes(self):
-        sum = 0
-        for at in range(len(self.dice)): 
-            if (self.dice[at] == 6):
-                sum = sum + 6
-        return sum
-    
+        
     @staticmethod
-    def score_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        at = 0
-        for at in range(6):
-            if (counts[6-at-1] == 2):
-                return (6-at)*2
+    def score_pair(*dices):
+        sorted_higuest_dice = sorted(dices, reverse=True)
+        for number in sorted_higuest_dice:
+            if sorted_higuest_dice.count(number) >= 2:
+                return number * 2
         return 0
-    
-    @staticmethod
-    def two_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        n = 0
-        score = 0
-        for i in range(6):
-            if (counts[6-i-1] >= 2):
-                n = n+1
-                score += (6-i)
-                    
-        if (n == 2):
-            return score * 2
-        else:
-            return 0
-    
-    @staticmethod
-    def four_of_a_kind( _1,  _2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[_1-1] += 1
-        tallies[_2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i+1) * 4
-        return 0
-    
 
     @staticmethod
-    def three_of_a_kind( d1,  d2,  d3,  d4,  d5):
-        t = [0]*6
-        t[d1-1] += 1
-        t[d2-1] += 1
-        t[d3-1] += 1
-        t[d4-1] += 1
-        t[d5-1] += 1
-        for i in range(6):
-            if (t[i] >= 3):
-                return (i+1) * 3
-        return 0
-    
-
-    @staticmethod
-    def smallStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[0] == 1 and
-            tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1):
-            return 15
-        return 0
-    
-
-    @staticmethod
-    def largeStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1
-            and tallies[5] == 1):
-            return 20
-        return 0
-    
-
-    @staticmethod
-    def fullHouse( d1,  d2,  d3,  d4,  d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
-
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-
-        for i in range(6):
-            if (tallies[i] == 2): 
-                _2 = True
-                _2_at = i+1
-            
-
-        for i in range(6):
-            if (tallies[i] == 3): 
-                _3 = True
-                _3_at = i+1
-            
-
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
+    def two_pair(*dice):
+        pairs = 0
+        number = 1
+        result = 0
+        while pairs <= 2 and number <= 6:
+            if dice.count(number) >= 2:
+                pairs += 1
+                result += number * 2
+            number += 1
+        if pairs == 2:
+            return result
         else:
             return 0
 
+    @staticmethod
+    def three_of_a_kind(*dice):
+        for number in dice:
+            if dice.count(number) >= 3:
+                return number * 3
+        return 0
+
+    @staticmethod
+    def four_of_a_kind(*dice):
+        for number in dice:
+            if dice.count(number) >= 4:
+                return number * 4
+        return 0
+
+    @staticmethod
+    def smallStraight(*dice):
+        for number in range(1,6):
+            if dice.count(number) != 1:
+                return 0
+        return Yatzy.chance(*dice)
+    
+    def largeStraight(*dice):
+        for number in range(2,7):
+            if dice.count(number) != 1:
+                return 0
+        return Yatzy.chance(*dice)
+        
+    @staticmethod
+    def fullHouse(*dice):
+        if Yatzy._pairHouse(*dice) and Yatzy.three_of_a_kind(*dice):
+            return Yatzy._pairHouse(*dice) + Yatzy.three_of_a_kind(*dice)
+        else:
+            return 0
+
+
+    def _pairHouse(*dice):
+        PAIR = 2
+        for number in range(6,0,-1):
+            if dice.count(number) == PAIR:
+                return PAIR * number
+        return 0
+
+
+
+        
